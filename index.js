2fe65d6d94f5d80789576a61583864f37c75fa57
Clemens Hemmerling <clemens@getkor.io>
2020-12-03 17:20:04 -0600

    fist commit

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event, country) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(`Hello from ${country}!`),
+    }
+    return response;
+};
+
