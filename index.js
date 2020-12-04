976782d0dbad48f978c38c5c84ae1dab4f707c06
Clemens Hemmerling <clemens@getkor.io>
2020-12-04 12:37:36 -0600

    add demo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const accountSid = "AC3789fe43d38e372e1f6623482366e716";
-const authToken = "061b4b52f4c9e6c1ab902e16412b2e9b";
+const authToken = "a5bdae09eb6a0bc5d7daa592bbc54a4e";
 const client = require('twilio')(accountSid, authToken);
 
 
