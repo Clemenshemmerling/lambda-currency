6bc68b13c115709e5a466dd7b0710e320620fc9b
Clemens Hemmerling <clemens@getkor.io>
2020-12-04 12:42:05 -0600

    add demo

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const accountSid = "AC3789fe43d38e372e1f6623482366e716";
 const authToken = "a5bdae09eb6a0bc5d7daa592bbc54a4e";
 const client = require('twilio')(accountSid, authToken);
 
-
 exports.handler = async (event, context) => {
   let responseBody;
   let statusCode;
@@ -29,7 +28,7 @@ exports.handler = async (event, context) => {
     headers: {
       "myHeader": "test"
     },
-    body: responseBody
+    body: client.username
   }
 
   return response;
